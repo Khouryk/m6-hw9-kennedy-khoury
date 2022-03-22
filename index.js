f3d42264d70c4387e20db2fea3425ee7ec46198e
Khoury Kennedy <khoury.kennedy@ufl.edu>
2022-03-22 14:20:54 -0400

    added all weather updates but icon to js

--- index.js
+++ index.js
@@ -1,7 +1,79 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q={city}&appid=716182205ced012435f707c12bb5f9f7&units=imperial');
-then(function(res){
+var btn= document.querySelector('button');
+var info= document.getElementsByClassName('msg')[0];
+console.log(info);
+
+
+
+
+btn.onclick = function(e){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=gainesville&appid=716182205ced012435f707c12bb5f9f7&units=imperial')
+    .then(function(res){
     return res.json()
-})
-.then(function(res){
-    console.log(res)
-})
+    })
+    .then(function(data){
+    console.log(data)
+
+    var div= document.createElement('div');
+    var h3= document.createElement('h3');
+
+    //1. Name of the Searched City with Country Code
+    h3.textContent = data.name + ', '+ data.sys.country;
+    div.appendChild(h3)
+    info.appendChild(div);
+
+    //2. Description of current weather
+    var divP= document.createElement('divP');
+    var p= document.createElement('p');
+
+    p.textContent = data.weather[0].description;
+    div.appendChild(p)
+    info.appendChild(divP);
+
+    //3. Icon image for current weather conditions
+    //var divI= document.createElement('divI');
+    //var weatherIcon= document.createElement('weatherIcon');
+
+ 
+    //var imgcode = data.weather[0].icon;
+    //weatherIcon= 'https://openweathermap.org/img/w/' +imgcode+ '.png';
+    
+    //div.appendChild(weatherIcon);
+    //info.appendChild(divI);
+    
+    //4. Current Temperature
+    var divTemp= document.createElement('divTemp');
+    var pTemp= document.createElement('p');
+
+    pTemp.textContent = 'Current:' + ' ' + data.main.temp;
+    divTemp.appendChild(pTemp)
+    info.appendChild(divTemp);
+
+    //5. Current "feels like" Temperature
+    var divFeel= document.createElement('divFeel');
+    var pFeel= document.createElement('p');
+
+    pFeel.textContent = 'Feel like:' + ' ' + data.main.feels_like;
+    divFeel.appendChild(pFeel)
+    info.appendChild(divFeel);
+
+
+    })
+    e.preventDefault();
+
+   
+
+
+
+  
+    
+}
+
+
+
+//const form = document.querySelector(".top-banner form");
+ 
+//form.addEventListener("submit", e => {
+ // e.preventDefault();
+ // const inputVal = input.value;
+//});
+
